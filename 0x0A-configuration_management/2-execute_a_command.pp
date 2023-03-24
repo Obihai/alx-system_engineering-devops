1ebddb9bc8bee1db7c0d97556f6d4c87aa969337
Kaegan Davis Haidome <obichi@coseo.com>
2023-03-24 19:02:28 +0100

    Executing a bash command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # This manifest kills a process named "killmenow" using the pkill command
-exec { 'killmenow':
-  command => 'pkill killmenow',
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
-
