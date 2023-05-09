894b126b05c5d3ceb7d1237624b5d8bd4c2135c2
Obihai <obichi@coseo.com>
2023-05-09 11:06:45 -0700

    Reddit api recursion

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Using recursion in API
+"""
+import requests
+
+def get_top_titles(subreddit):
+    """
+    Returns a list with the titles of the top ten posts in a subreddit,
+    obtained using Reddit's API.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "my_api_client"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()["data"]["children"]
+        titles = [post["data"]["title"] for post in data]
+        return titles
+    else:
+        return None
