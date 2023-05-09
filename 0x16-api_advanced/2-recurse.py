5579ae47a9e2feb8ab64abe945c3a1ad124a7381
Kaegan Davis Haidome <obichi@coseo.com>
2023-05-09 19:33:53 +0100

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,17 +12,17 @@ def recurse(subreddit, hot_list=[], after=None):
     parameters = {'after': after}
     results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
-    
+
     if results.status_code == 200:
         data = results.json().get('data')
-        after_data = data.get('after')        
+        after_data = data.get('after')
         if after_data:
             recurse(subreddit, hot_list, after=after_data)
-            
+
         all_titles = data.get('children')
         for title_ in all_titles:
             hot_list.append(title_['data']['title'])
-            
+
         return hot_list
     else:
         return None
