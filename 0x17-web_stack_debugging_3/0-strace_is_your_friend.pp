5eea98c820438d3706da13d909ee3be743a39286
Kaegan Davis Haidome <obichi@coseo.com>
2023-05-10 14:09:49 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
-# A puppet manuscript to replace a line in a file on a server
+# A Puppet manifest to replace a line in a file on a server
 
 $file_to_edit = '/var/www/html/wp-settings.php'
 
 file_line { 'replace_line':
-  ensure  => present,
-  path    => $file_to_edit,
-  line    => 'define("DB_PASSWORD", "php");',
-  match   => '^define\("DB_PASSWORD", "phpp"\);',
+  ensure => present,
+  path   => $file_to_edit,
+  line   => 'define("DB_PASSWORD", "php");',
+  match  => '^define\("DB_PASSWORD", "phpp"\);',
 }
