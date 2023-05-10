7adb284d4734b3dc26670ec87cb74ff6723b9b2f
Obihai <obichi@coseo.com>
2023-05-10 06:00:08 -0700

    Replace a line in a file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+file_line { 'replace_line':
+  ensure  => present,
+  path    => $file_to_edit,
+  line    => 'define("DB_PASSWORD", "php");',
+  match   => '^define\("DB_PASSWORD", "phpp"\);',
+}
